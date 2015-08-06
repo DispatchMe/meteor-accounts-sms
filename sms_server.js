f72beb5304924364821bd1c0251f73396ac96e8e
Gwen <gwendall.esnault@gmail.com>
2015-08-06 10:19:11 +0200

    ensure the verif code is 4-digit long

--- sms_server.js
+++ sms_server.js
@@ -3,7 +3,6 @@ var codes = new Mongo.Collection('meteor_accounts_sms');
 Meteor.methods({
   'accounts-sms.sendVerificationCode': function (phone) {
     check(phone, String);
-
     return Accounts.sms.sendVerificationCode(phone);
   }
 });
@@ -74,7 +73,7 @@ Accounts.sms.sendVerificationCode = function (phone) {
     throw new Meteor.Error('not a mobile number');
   }
 
-  var code = Math.floor(Random.fraction() * 10000) + '';
+  var code = (Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000) + '';
   var message = Accounts.sms.message.replace(/{{code}}/g, code);
 
   // Create user if does not exist
